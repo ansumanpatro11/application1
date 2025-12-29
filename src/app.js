92df156aeaea4627b615fdc829809656c74036a6
ansumanpatro11 <122ee0834@nitrkl.ac.in>
2025-12-29 12:16:10 +0530

    all setup done

--- src/app.js
+++ src/app.js
@@ -4,14 +4,20 @@ import cookieParser from 'cookie-parser';
 const app=express();
 
 
-app.use(cors(){
+app.use(cors({
     origin:process.env.CROSS_ORIGIN,
     credentials:true
-})
+}))
 
 app.use(express.json({limit:'10kb'}));
 app.use(express.urlencoded({extended:true,limit:'10kb'})); //to encode urls
 app.use(express.static('public'));  //images, assets etc
 app.use(cookieParser())
 
-export {app}
+//routes
+
+import userRouter from './routes/user.routes.js';
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
+export default app
